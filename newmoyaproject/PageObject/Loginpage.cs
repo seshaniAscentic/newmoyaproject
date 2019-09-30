e92bfb523c324f282aaf2be843f5e75265f33480
Seshani-Ascentic <Seshani@ascentic.se>
2019-09-30 15:20:23 +0530

    AUT-11- created seperate folders for base and test classes

--- newmoyaproject/PageObject/Loginpage.cs
+++ newmoyaproject/PageObject/Loginpage.cs
@@ -1,10 +1,32 @@
 ﻿using System;
+using NUnit.Framework;
 using System.Collections.Generic;
 using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+
 
 namespace newmoyaproject.PageObject
 {
-    class Loginpage
+       public class Loginpage
     {
+        
+        public IWebDriver driver;
+        public Loginpage()
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\Chromedrivers");
+            driver.Url = "https://moya-admin-web-dev.azurewebsites.net/";
+            driver.Manage().Window.Maximize();
+            
+
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
     }
+
+    
 }
---
+++ newmoyaproject/Tests/login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using newmoyaproject.PageObject;
+
+namespace newmoyaproject.PageObject
+{
+  public class login 
+    {
+
+    }
+}
