280ca8f7de7bccdfe9633585e884e855a8df1d7d
Seshani-Ascentic <Seshani@ascentic.se>
2019-10-04 12:41:28 +0530

    updated packages and changed test class

--- newmoyaproject/PageObject/Loginpage.cs
+++ newmoyaproject/PageObject/Loginpage.cs
@@ -6,27 +6,47 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support;
 
-
 namespace newmoyaproject.PageObject
-{
-       public class Loginpage
+{ 
+    public class SetupApplication
     {
-        
-        public IWebDriver driver;
-        public Loginpage()
+        private IWebDriver driver;
+        //public IWebDriver driver
+        //{ get;
+        //  set;
+        //}
+        class Loginpage
         {
-            IWebDriver driver = new ChromeDriver(@"C:\Chromedrivers");
-            driver.Url = "https://moya-admin-web-dev.azurewebsites.net/";
-            driver.Manage().Window.Maximize();
-            
-
-        }
-        [Test]
-        public void Test2()
-        {
-            Assert.Pass();
+            public void Loginurl()
+            {
+                IWebDriver driver = new ChromeDriver(@"C:\Chromedrivers");
+                driver.Url = "https://moya-admin-web-dev.azurewebsites.net/";
+                driver.Manage().Window.Maximize();
+            }
+            class Credentials
+            {
+                public IWebDriver driver;
+                public void Username()
+                {
+                    driver.FindElement(By.Id("i0116")).SendKeys("userEmail");
+                }
+                public void Password()
+                {
+                    driver.FindElement(By.Id("idSIButton9")).SendKeys("userpassword");
+                }
+                public void Btnsubmit()
+                {
+                    driver.FindElement(By.XPath("//*[@type='submit']"));
+                }
+                //public void Logintosystem(String userEmail, String userpassword)
+                //{
+                //        this.username(userEmail);
+                //        this.password(userpassword);
+                //}
+            }
         }
     }
-
-    
 }
+
+
+
--- newmoyaproject/Tests/login.cs
+++ newmoyaproject/Tests/login.cs
@@ -9,8 +9,22 @@ using newmoyaproject.PageObject;
 
 namespace newmoyaproject.PageObject
 {
-  public class login 
+    public class Logindetails : SetupApplication
     {
+        //private object Login;
 
+        //new IWebDriver driver;
+
+        public void Enterlogindetails()
+        {
+
+           // Login.Logintosystem("seshani@ascentic.se", "Thanushka@123");
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
     }
 }
+
